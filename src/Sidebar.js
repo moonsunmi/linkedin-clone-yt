fe1f984b7c14636c1bc1af89ff096baa5fc84597
moonsunmi <celebmoon@naver.com>
2022-10-17 21:12:54 +0900

    sidebar 완성

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,10 +3,19 @@ import "./Sidebar.css";
 import { Avatar } from "@mui/material";
 
 const Sidebar = () => {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt="" />
+        <img
+          src="https://drive.google.com/uc?export=view&id=1xeTuuFf5NNugOgSf-2yjtUXi3whybZp2"
+          alt=""
+        />
         <Avatar className="sidebar__avatar" />
         <h2>Sonny Sangha</h2>
         <h4>sonny.sangha@gmail.com</h4>
@@ -14,16 +23,21 @@ const Sidebar = () => {
 
       <div className="sidebar__stats">
         <div className="sidebar__stat">
-          <p>Who vied you</p>
+          <p>Who viwed you</p>
           <p className="sidebar__statNumber">2,543</p>
         </div>
-        <p>Views on post</p>
-        <div className="sidebar__stat"></div>
-        <p className="sidebar__statNumber">2,400</p>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,400</p>
+        </div>
       </div>
 
       <div className="sidebar__bottom">
         <p>Recent</p>
+        {recentItem("reactJs")}
+        {recentItem("programming")}
+        {recentItem("softwareenginering")}
+        {recentItem("developer")}
       </div>
     </div>
   );
